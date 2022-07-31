0ef86289f238cb6364f9ab4d115f0241c19163a2
isyoo1213 <moethns@gmail.com>
2022-07-31 19:44:11 +0900

    feat: Add CartItem Component for showing items on Cart

--- OrderApp/src/conponents/Cart/Cart.js
+++ OrderApp/src/conponents/Cart/Cart.js
@@ -1,12 +1,33 @@
+import React, { useContext } from "react";
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalPrice = `$${cartCtx.totalPrice.toFixed(2)}`;
+
+  const hasCartItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
-    <ul className={styles['cart-items']}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={styles["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler(null, item)}
+        />
       ))}
+      {/* 메서드를 binding하는 정확한 이유는..?  */}
     </ul>
   );
 
@@ -15,11 +36,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>999999</span>
+        <span>{totalPrice}</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
-        <button className={styles.button}>Order</button>
+        <button className={styles["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasCartItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
