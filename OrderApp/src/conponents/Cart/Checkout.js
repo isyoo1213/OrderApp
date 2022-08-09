02497d61ee75a8260a1135a5aaf0ea1ed6140c12
isyoo1213 <moethns@gmail.com>
2022-08-09 16:32:35 +0900

    feat: Add features for rough input validation

--- OrderApp/src/conponents/Cart/Checkout.js
+++ OrderApp/src/conponents/Cart/Checkout.js
@@ -1,7 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Checkout.module.css";
 
+const isEmpty = (value) => {
+  return value.trim() === "";
+};
+const isFiveChars = (value) => {
+  return value.trim().length === 5;
+};
+
 const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    postal: true,
+    address: true,
+    detailAddress: true,
+  });
+
   const nameInputRef = useRef();
   const postalInputRef = useRef();
   const addressInputRef = useRef();
@@ -15,26 +29,57 @@ const Checkout = (props) => {
     const enteredAddress = addressInputRef.current.value;
     const enteredDetailAddress = detailAddressInputRef.current.value;
 
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredpostalIsValid = isFiveChars(enteredPostal);
+    const enteredAddressIsValid = !isEmpty(enteredAddress);
+    const enteredDetailAddressIsValid = !isEmpty(enteredDetailAddress);
+
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      postal: enteredpostalIsValid,
+      address: enteredAddressIsValid,
+      detailAddress: enteredDetailAddressIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredpostalIsValid &&
+      enteredAddressIsValid &&
+      enteredDetailAddressIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
   };
+
+  const inputControlStyles = (inputValidity) => {
+    return `${styles.control} ${inputValidity ? '' : styles.invalid}`
+  }
 
   return (
     <form className={styles.form} onSubmit={confirmHandler}>
-      <div className={styles.control}>
+      <div className={inputControlStyles(formInputsValidity.name)}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid Name!</p>}
       </div>
-      <div className={styles.control}>
+      <div className={inputControlStyles(formInputsValidity.postal)} >
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalInputRef} />
         {/* default null을 피하기 위해 text 타입으로 설정 */}
+        {!formInputsValidity.postal && <p>Please enter a valid Postal Code!</p>}
       </div>
-      <div className={styles.control}>
+      <div className={inputControlStyles(formInputsValidity.address)}>
         <label htmlFor="address">Address</label>
         <input type="text" id="address" ref={addressInputRef} />
+        {!formInputsValidity.address && <p>Please enter a valid Address!</p>}
       </div>
-      <div className={styles.control}>
+      <div className={inputControlStyles(formInputsValidity.detailAddress)}>
         <label htmlFor="detailAddress">Detail Address</label>
         <input type="text" id="detailAddress" ref={detailAddressInputRef} />
+        {!formInputsValidity.detailAddress && (
+          <p>Please enter a valid Detail Address!</p>
+        )}
       </div>
       <div className={styles.actions}>
         <button type="button" onClick={props.onCancel}>
